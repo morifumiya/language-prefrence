1152bdde1e7eff739ebbf38c96d9bf0b64d113c9
morifumiya <morimorimorifu@gmail.com>
2023-03-22 18:17:50 +0900

    begin :🎉 プロジェクトの開始

---
+++ app/src/main/java/rob/myappcompany/languageprefrences/MainActivity.java
@@ -0,0 +1,84 @@
+package rob.myappcompany.languageprefrences;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    TextView textView;
+
+    public void setLanguage(String language) {
+        sharedPreferences.edit().putString("language", language).apply();
+        textView.setText(language);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.english:
+                setLanguage("English");
+                return true;
+            case R.id.spanish:
+                setLanguage("Spanish");
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        sharedPreferences = this.getSharedPreferences("rob.myappcompany.languageprefrences", MODE_PRIVATE);
+
+        String language = sharedPreferences.getString("language", "Error");
+
+        Log.i("the language", language);
+
+        if (language.equals("Error")) {
+
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_btn_speak_now)
+                    .setTitle("Choose a language")
+                    .setMessage("Which language would you like to use?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // Set English
+                            setLanguage("English");
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // Set Spanish
+                            setLanguage("Spanish");
+                        }
+                    })
+                    .show();
+        } else {
+            textView.setText(language);
+        }
+    }
+}
